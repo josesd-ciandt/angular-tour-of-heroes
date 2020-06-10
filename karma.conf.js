2b95f600d2165bd710478591cc1c454ee2419b67
José Ricardo da Silva Dainese <josesd@ciandt.com>
2020-06-09 23:15:43 -0300

    Configure Karma Config

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+const os = require('os');
+const chromeHeadlessSupported = os.platform() !== 'win32' || Number((os.release().match(/^(\d+)/) || ['0', '0'])[1]) >= 10;
 
 module.exports = function (config) {
   config.set({
@@ -8,41 +8,37 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
+      require('karma-mocha-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-tour-of-heroes'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      dir: require('path').join(__dirname, '../../coverage'),
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['mocha','coverage'],
+    coverageReporter: {
+      type: 'text-summary'
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    browserDisconnectTimeout: 10000,
-    browserDisconnectTolerance: 3,
-    browserNoActivityTimeout: 60000,
-    failOnEmptyTestSuite: false,
-    restartOnFileChange: true,
+    browserNoActivityTimeout: 30000,
+    browsers: [
+      chromeHeadlessSupported ? 'ChromeHeadless' : 'Chrome'
+    ],
     customLaunchers: {
-    ChromeHeadlessNoSandbox: {
-      base: 'ChromeHeadless',
-       flags: [
-      '--headless',
-      '--disable-gpu',
-      '--disable-translate',
-      '--disable-extensions',
-      '--no-sandbox',  // Added to fix an issue where of Failed to connect to chrome browser
-     '--remote-debugging-port=9876',
-    ]
-    }
-    }
+      ChromeHeadless: {
+          base: 'Chrome',
+          flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
+      }
+    },
+    singleRun: true
   });
 };
